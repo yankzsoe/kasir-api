4e43d2e4fdc1511a8500800b9a2c76f1621aaf13
Yayang Suryana <suryana@ebconnect.com>
2026-01-25 23:26:05 +0700

    enable https on swagger

--- docs/docs.go
+++ docs/docs.go
@@ -373,7 +373,7 @@ var SwaggerInfo = &swag.Spec{
 	Version:          "1.0",
 	Host:             "localhost:8080",
 	BasePath:         "/api/v1",
-	Schemes:          []string{"http"},
+	Schemes:          []string{"http", "https"},
 	Title:            "Kasir API",
 	Description:      "A complete Point of Sale (POS) API built with Go, Gin, and GORM",
 	InfoInstanceName: "swagger",
